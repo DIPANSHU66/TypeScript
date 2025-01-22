611c628064ce58647054185dba799bea324c5965
DIPANSHU66 <dipanshu682004@gmail.com>
2025-01-22 18:04:56 +0530

    learning

--- Basics/app.js
+++ Basics/app.js
@@ -1,7 +1,13 @@
-function add(num1, num2, r, s) {
-    if (r)
-        console.log("".concat(s, "  &{num1+num2}"));
-    else
-        return num1 + num2;
-}
-console.log(add(1, 2, true, 'sum of two  number is'));
+// function add(num1:number,num2:number ,r :boolean,s:string){
+//     if(r)  console.log(`${s}`);
+//      else return num1+num2;
+// }
+// add(1,2,true,'sum of two  number is');
+console.log('lets learn pbjcect');
+var person = {
+    firstname: "dipanshu",
+    age: 21,
+    skills: ['reactjs', 'nodejs'],
+};
+console.log(person);
+var language = "english";
--- Basics/app.ts
+++ Basics/app.ts
@@ -1,7 +1,45 @@
-function add(num1:number,num2:number ,r :boolean,s:string){
+// // function add(num1:number,num2:number ,r :boolean,s:string){
 
-    if(r)  console.log(`${s}`);
-     else return num1+num2;
-}
+// //     if(r)  console.log(`${s}`);
+// //      else return num1+num2;
+// // }
 
-add(1,2,true,'sum of two  number is');
+// // add(1,2,true,'sum of two  number is');
+
+
+// console.log('lets learn pbjcect');
+
+//     var person:{
+//     firstname:string;
+//     age:number;
+//     skills:string[];
+// }={
+//     firstname:"dipanshu",
+//     age:21,
+//     skills:['reactjs','nodejs'],
+// };
+// console.log(person);
+// var language:string="english";
+// // let languae:string[];
+
+
+
+
+// var person :{
+//     name:string;
+//     age:number;
+//     skills:string[];
+//     product:[number,string];
+//     role:string;
+
+// }={
+//     name:"rahul",
+//     age:1,
+//     skills:["React","Node"],
+//     product:[1,"macbook"],
+//     role:'author'
+// }
+// person.product[0]=3;
+
+// enum role {ADMIN,AUTHOR,READ_USER_ONLY};
+
